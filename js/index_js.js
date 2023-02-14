db600456f0f41d380f779de81ad2a3246ab52e85
tkarkrgud153 <tkarkrgud1532@gmail.com>
2023-02-14 12:54:38 +0900

    first commit

---
+++ js/index_js.js
@@ -0,0 +1,8 @@
+function viewTabsOn() {
+    let v=document.getElementById("myTabs");
+    if (v.style.display == 'none'){
+        v.style.display = 'block';
+    }else{
+        v.style.display = 'none';
+    }
+} 
